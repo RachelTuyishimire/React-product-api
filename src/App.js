1f5d28fff4b07478e9666b045260a8d34104176a
student <you@example.com>
2023-07-06 09:03:37 +0300

    Products Pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Products from './Shop';
+
+function App() {
+  return (
+    <div>
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
